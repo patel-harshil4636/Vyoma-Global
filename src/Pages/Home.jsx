240b3fed680a928890e5f79ec982708b924a0a7d
patel-harshil4636 <patelharshil4636@gmail.com>
2025-10-30 03:26:51 +0530

    Updated frontend code

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Nav from '../components/Home/Nav'
+import Hero from '../components/Home/Hero'
+import Main from '../components/Home/Main'
+import Footer from '../components/Home/Footer'
+
+function Home() {
+  return (
+   <>
+      <Nav></Nav>
+      <Hero></Hero>
+      <Main></Main>
+      <Footer></Footer>
+   </>
+  )
+}
+
+export default Home
